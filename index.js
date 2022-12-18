948054a69c59df4f02ad8715e9bbd16658e8bbd4
ronieo <romelzen50@gmail.com>
2022-12-18 21:04:57 +0900

    feat: add multiple seven in index.js

--- index.js
+++ index.js
@@ -4,5 +4,16 @@
   /*5의 배수*/
 
   /*7의 배수*/
+var userNumber = prompt("숫자를 입력하세요.");
+var displayArea = document.querySelector("#result");
+
+if (userNumber != null){
+  if(userNumber % 7 === 0){
+    displayArea.innerHTML = userNumber + "은 7의 배수입니다!";
+  }
+  else{
+    displayArea.innerHTML = userNumber + "은 7의 배수가 아닙니다!";
+  }
+}
 
 </javascript>
